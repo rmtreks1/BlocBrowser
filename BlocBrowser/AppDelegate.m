0a2171544a6973d833f190bcee88a079ba2c65bf
Roshan Mahanama <roshan@Roshans-MacBook-Pro.local>
2015-04-14 19:43:35 +1000

    clears history when app is closed

--- BlocBrowser/AppDelegate.m
+++ BlocBrowser/AppDelegate.m
@@ -23,12 +23,23 @@
     self.window.backgroundColor = [UIColor whiteColor];
      self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BLCWebBrowserViewController alloc] init]];
     [self.window makeKeyAndVisible];
+    
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Welcome", @"welcome title") message:NSLocalizedString(@"awesome browser for surfing stuff your wife shouldn't know about", @"welcome comment") delegate:nil cancelButtonTitle:NSLocalizedString(@"Go!", @"welcome button title") otherButtonTitles:nil];
+    
+    [alert show];
+    
+    
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    UINavigationController *navigationVC = (UINavigationController *)self.window.rootViewController;
+    BLCWebBrowserViewController *browserVC = [[navigationVC viewControllers] firstObject];
+    [browserVC resetWebView];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
