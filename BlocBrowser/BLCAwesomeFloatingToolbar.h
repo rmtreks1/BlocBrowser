c764bbc2c12778badb39f6eba0df0a8008c218b1
Roshan Mahanama <roshan@Roshans-MacBook-Pro.local>
2015-04-15 09:35:43 +1000

    all code entered as per checkout

---
+++ BlocBrowser/BLCAwesomeFloatingToolbar.h
@@ -0,0 +1,29 @@
+//
+//  BLCAwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Roshan Mahanama on 14/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCAwesomeFloatingToolbar;
+
+@protocol BLCAwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(BLCAwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface BLCAwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <BLCAwesomeFloatingToolbarDelegate> delegate;
+
+@end
